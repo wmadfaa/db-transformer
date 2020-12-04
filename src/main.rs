3c10bd6beceeb2eaaea4dd26831ed7356c2cb840
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-12-04 18:25:18 +0100

    add  delete_product fn

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@
 extern crate diesel;
 extern crate dotenv;
 
+use self::models::{NewProduct, NewSale, Product, Sale};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
+
+pub mod models;
+pub mod schema;
 
 fn establish_connections() -> PgConnection {
     dotenv().ok();
@@ -13,6 +17,27 @@ fn establish_connections() -> PgConnection {
     PgConnection::establish(&db_url).expect(&format!("Error connecting to {}", db_url))
 }
 
+fn create_product<'a>(conn: &PgConnection, id: i32, category: &'a str, name: &'a str) -> Product {
+    use schema::products;
+
+    let new_product = NewProduct { id, category, name };
+
+    diesel::insert_into(products::table)
+        .values(new_product)
+        .get_result(conn)
+        .expect(&format!("Error saving new product with name '{}'", name))
+}
+
+fn delete_products(conn: &PgConnection) {
+    use schema::products;
+
+    diesel::delete(products::table)
+        .execute(conn)
+        .expect("Error deleting products");
+}
+
 fn main() {
     let conn = establish_connections();
+    delete_products(&conn);
+    create_product(&conn, 2, "apple", "iphone_");
 }
---
+++ src/models.rs
@@ -0,0 +1,38 @@
+use super::schema::{products, sales};
+
+#[derive(Identifiable, Queryable)]
+#[table_name = "products"]
+pub struct Product {
+    pub id: i32,
+    pub category: String,
+    pub name: String,
+}
+
+#[derive(Identifiable, Queryable, Associations)]
+#[belongs_to(Product)]
+#[table_name = "sales"]
+pub struct Sale {
+    pub id: String,
+    pub product_id: i32,
+    pub sale_date: i64,
+    pub quantity: f64,
+    pub unit: String,
+}
+
+#[derive(Insertable)]
+#[table_name = "products"]
+pub struct NewProduct<'a> {
+    pub id: i32,
+    pub category: &'a str,
+    pub name: &'a str,
+}
+
+#[derive(Insertable)]
+#[table_name = "sales"]
+pub struct NewSale<'a> {
+    pub id: String,
+    pub product_id: &'a i32,
+    pub sale_date: &'a i64,
+    pub quantity: &'a f64,
+    pub unit: &'a str,
+}
